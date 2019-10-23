7b967024cb831839003dd6799aa2529f0cd2cd77
Matija Almasi <almasimatija@gmail.com>
2019-10-23 17:22:22 +0200

    cherry pick konflikt rijesen

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,7 +6,11 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+<<<<<<< HEAD
 	printf("Pozdrav iz aplikacje od [Anto Tufekoviæ], R3999");
+=======
+	printf("Pozdrav iz aplikacje od [Anto Tufekoviæ]");
+>>>>>>> 73d917a... zad1
 	return 0;
 }
 
